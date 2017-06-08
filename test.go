b2a1a111ab0816f37d7288d6548d35fdbb893932
mixcode <mixcode@users.noreply.github.com>
2017-06-08 15:41:46 +0900

    Reset and revived

---
+++ test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func run() (err error) {
+	// add a new comment line
+	// 日本語普通に行けるはず
+	fmt.Println("hi")
+
+	return
+}
+
+func main() {
+	var err error
+
+	err = run()
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
